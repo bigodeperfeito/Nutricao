0228b486f1781b6c04cb1710d462f3d98afa3673
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2025-10-03 11:03:28 -0300

    d

--- js/principal.js
+++ js/principal.js
@@ -7,7 +7,7 @@
     
 
      var paciente = document.querySelector("#primeiro-paciente");
-     var tdPeso = paciente.querySelector(".info-peso");
+     var tdPeso = document.querySelector(".info-peso");
      console.log(paciente);
      console.log(tdPeso);
 
