e3e1355a0b47a56894568b79efcfe5ec692c4fea
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2025-10-07 09:50:19 -0300

    d

--- js/principal.js
+++ js/principal.js
@@ -1,21 +1,26 @@
 
-  
      var titulo = document.querySelector("h1");
      console.log(titulo);
      console.log(titulo.textContent);
      titulo.textContent=("Curintia nutrição");
+
+     var paciente = document.querySelector("primeir paciente");
+     var tdNome = document.querySelector(".info nome");
+     var nome = tdNome.textContent;
+     console.log(nome);
+
     
 
-     var paciente = document.querySelector("#primeiro-paciente");
+    
      var tdPeso = document.querySelector(".info-peso");
      var peso = tdPeso.textContent;
-     console.log(paciente);
      console.log(peso);
 
-     var tdAltura = document.querySelector("#primeiro-paciente");
-     var altura = document.querySelector(".info-altura");
+    
+     var tdAltura = document.querySelector(".info-altura");
      var altura = tdAltura.textContent;
      console.log(altura);
      
    
-    
+     var imc = peso / (altura * altura) ;
+     console.log(imc);
