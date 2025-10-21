af0808767b41e8682a7a1a1f4f4e7f1d69c085f9
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2025-10-21 09:51:24 -0300

    4

--- js/principal.js
+++ js/principal.js
@@ -8,14 +8,13 @@ for (var i = 0; i < pacientes.lengt; i++) {
 
      var paciente = pacientes[i];
 
-     var tdPeso = document.querySelector(".info-peso");
+     var tdPeso = paciente.querySelector(".info-peso");
      var peso = tdPeso.textContent;
 
-     var tdAltura = document.querySelector(".info-altura");
+     var tdAltura = paciente.querySelector(".info-altura");
      var altura = tdAltura.textContent;
 
-     var tdImc = document.querySelector(".info-imc");
-
+     var tdImc = paciente.querySelector(".info-imc");
 
      var pesoEhValido = true;
      var alturaEhValida = true;
@@ -31,7 +30,6 @@ for (var i = 0; i < pacientes.lengt; i++) {
           alturaEhValida = false;
           tdImc.textContent = "Altura invalida";
      }
-
 
      if (alturaEhValida && pesoEhValido) {
           var imc = peso / (altura * altura);
