a9423bc4d4eddb8e05416ed7692c8ae56519e690
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2025-11-07 10:57:32 -0300

    s

--- js/form.js
+++ js/form.js
@@ -1,40 +1,15 @@
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-event.preventDefault();
-//alert("Oi eu sou o botão e fui clicado");
+//console.log(document.querySelector("h1"));
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = ("White nutritions");
 
+var pacientes = document.querySelectorAll(".paciente");
 
-var form = document.querySelector("#form-adiciona");
+for(var i = 0; i < pacientes.length; i++) {
 
-var nome = form.nome.value;
-var peso = form.peso.value;
-var altura = form.altura.value;
-var gordura = form.gordura.value;
+var paciente = pacientes[i];
 
-var pacienteTr = document.createElement("tr");
-
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
-
-
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-//imcTd.textContent = imc;
-
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd);
-pacienteTr.appendChild(gorduraTd);
-
-var tabela = document.querySelector("#tabela-pacientes");
-tabela.appendChild(pacienteTr);
-
-});
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
@@ -61,54 +36,62 @@ paciente.classList.add("paciente-invalido");
 }
 
 if (alturaEhValido && pesoEhValido){
-var imc = peso / (altura * altura);
+var imc = calculaImc(peso, altura);
 tdImc.textContent = imc.toFixed(2);
 }
-
-
-
-titulo.addEventListener("click", mostraMensagem);
-function mostraMensagem(){
-alert("Olá eu fui clicado.")
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-event.preventDefault();
-//alert("Oi eu sou o botão e fui clicado");
+function calculaImc (peso, altura){
+var imc = 0;
+var imc = peso / (altura * altura);
+return imc.toFixed(2);
+}
 
+//console.log(document.querySelector("h1"));
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = ("White nutritions");
 
-var form = document.querySelector("#form-adiciona");
+var pacientes = document.querySelectorAll(".paciente");
 
-var nome = form.nome.value;
-var peso = form.peso.value;
-var altura = form.altura.value;
-var gordura = form.gordura.value;
+for(var i = 0; i < pacientes.length; i++) {
 
+var paciente = pacientes[i];
 
-var pacienteTr = document.createElement("tr");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
+var tdImc = paciente.querySelector(".info-imc");
 
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calculaImc(peso, altura);
+var pesoEhValido = true;
+var alturaEhValido = true;
 
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd);
-pacienteTr.appendChild(gorduraTd);
-pacienteTr.appendChild(ImcTd);
+if (peso <= 0 || peso >= 1000){
+console.log("Peso inválido!");
+pesoEhvalido = false;
+tdImc.textContent = "Peso inválido!";
+paciente.classList.add("paciente-invalido");
+}
 
+if (altura <= 0 || altura >= 3.00){
+console.log("Altura inválida!");
+alturaEhValido = false;
+tdImc.textContent = "Altura inválida!";
+paciente.classList.add("paciente-invalido");
+}
 
-var tabela = document.querySelector("#tabela-pacientes");
-tabela.appendChild(pacienteTr);
+if (alturaEhValido && pesoEhValido){
+var imc = calculaImc(peso, altura);
+tdImc.textContent = imc.toFixed(2);
+}
+}
 
-});
+function calculaImc (peso, altura){
+var imc = 0;
+var imc = peso / (altura * altura);
+return imc.toFixed(2);
+}
