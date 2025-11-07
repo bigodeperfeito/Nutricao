195b8600504d3670fd1fff89bee574f6c509c545
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2025-11-07 11:17:18 -0300

    5

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -2,7 +2,7 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("White nutritions");
+titulo.textContent = ("curintia nutritions");
 
 var pacientes = document.querySelectorAll(".paciente");
 
