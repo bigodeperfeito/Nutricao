a13ffd6a672dfd06aa45ecde4a5229a8ce571b9f
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2025-11-11 08:52:28 -0300

    a

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -2,96 +2,53 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("curintia nutritions");
+titulo.textContent = ("Nego nutritations");
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for (var i = 0; i < pacientes.length; i++) {
+for(var i = 0; i < pacientes.length; i++) {
 
-    var paciente = pacientes[i];
+var paciente = pacientes[i];
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-    var tdImc = paciente.querySelector(".info-imc");
+var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValido = true;
+var pesoEhValido = true;
+var alturaEhValido = true;
 
-    if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
-        pesoEhvalido = false;
-        tdImc.textContent = "Peso inválido!";
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida!");
-        alturaEhValido = false;
-        tdImc.textContent = "Altura inválida!";
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (alturaEhValido && pesoEhValido) {
-        var imc = calculaImc(peso, altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
+if (peso <= 0 || peso >= 1000){
+console.log("Peso inválido!");
+pesoEhvalido = false;
+tdImc.textContent = "Peso inválido!";
+paciente.classList.add("paciente-invalido");
 }
 
-function calculaImc(peso, altura) {
-    var imc = 0;
-    var imc = peso / (altura * altura);
-    return imc.toFixed(2);
+if (altura <= 0 || altura >= 3.00){
+console.log("Altura inválida!");
+alturaEhValido = false;
+tdImc.textContent = "Altura inválida!";
+paciente.classList.add("paciente-invalido");
 }
 
-//console.log(document.querySelector("h1"));
-var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent = ("White nutritions");
-
-var pacientes = document.querySelectorAll(".paciente");
-
-for (var i = 0; i < pacientes.length; i++) {
-
-    var paciente = pacientes[i];
-
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-
-    var tdImc = paciente.querySelector(".info-imc");
-
-    var pesoEhValido = true;
-    var alturaEhValido = true;
-
-    if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
-        pesoEhvalido = false;
-        tdImc.textContent = "Peso inválido!";
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida!");
-        alturaEhValido = false;
-        tdImc.textContent = "Altura inválida!";
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (alturaEhValido && pesoEhValido) {
-        var imc = calculaImc(peso, altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
+if (alturaEhValido && pesoEhValido){
+var imc = calculaImc (peso, altura);
+tdImc.textContent = imc.toFixed(2);
+}
 }
 
-function calculaImc(peso, altura) {
-    var imc = 0;
-    var imc = peso / (altura * altura);
-    return imc.toFixed(2);
+
+titulo.addEventListener("click", mostraMensagem);
+function mostraMensagem(){
+alert("Olá eu fui clicado.")
+}
+
+function calculaImc (peso, altura){
+var imc = 0;
+var imc = peso / (altura * altura);
+return imc;
 }
